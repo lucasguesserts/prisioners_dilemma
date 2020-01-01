ac3b0eb5dd74e9b797c112e566f2c2666c6ce5cc
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2020-01-01 11:37:55 -0300

    Match class fixed.

--- ChampionshipLib/include/Match.hpp
+++ ChampionshipLib/include/Match.hpp
@@ -1,18 +1,20 @@
 #ifndef MATCH_HPP
 #define MATCH_HPP
 
-//#include <vector>
-//#include <Player.hpp>
+#include <vector>
+#include <Strategies.hpp>
+#include <Payoff.hpp>
+#include <Player.hpp>
 
-//class Match
-//{
-//	public:
-//		Match(Player& left, Player& right);
-//
-//		static constexpr unsigned numberOfTurns = 5;
-//		Player& leftPlayer, rightPlayer;
-//		std::vector<Decision> leftDecisions, rightDecisions;
-//		std::vector<Payoff>   leftPayoff,    rightPayoff;
-//};
+class Match
+{
+	public:
+		Match(Player& left, Player& right);
+
+		static constexpr unsigned numberOfTurns = 5;
+		Player &leftPlayer, &rightPlayer;
+		std::vector<Decision> leftDecisions, rightDecisions;
+		std::vector<Payoff>   leftPayoff,    rightPayoff;
+};
 
 #endif
--- ChampionshipLib/source/Match.cpp
+++ ChampionshipLib/source/Match.cpp
@@ -1,19 +1,19 @@
 #include <Match.hpp>
 
-//Match::Match(Player& left, Player& right)
-//	: leftPlayer(left),
-//	  rightPlayer(right)
-//{
-//	leftDecisions.reserve(Match::numberOfTurns);
-//	rightDecisions.reserve(Match::numberOfTurns);
-//	leftPayoff.reserve(Match::numberOfTurns);
-//	rightPayoff.reserve(Match::numberOfTurns);
-//	for(unsigned turn=0 ; turn<Match::numberOfTurns ; ++turn)
-//	{
-//		leftDecisions.push_back(   leftPlayer.strategy->makeDecision(leftDecisions,  rightDecisions) );
-//		rightDecisions.push_back( rightPlayer.strategy->makeDecision(rightDecisions, leftDecisions)  );
-//		leftPayoff.push_back(    PayoffComputer::left (leftDecisions.back(), rightDecisions.back()) );
-//		leftPayoff.push_back(    PayoffComputer::right(leftDecisions.back(), rightDecisions.back()) );
-//	}
-//	return;
-//}
+Match::Match(Player& left, Player& right)
+	: leftPlayer(left),
+	  rightPlayer(right)
+{
+	leftDecisions.reserve(Match::numberOfTurns);
+	rightDecisions.reserve(Match::numberOfTurns);
+	leftPayoff.reserve(Match::numberOfTurns);
+	rightPayoff.reserve(Match::numberOfTurns);
+	for(unsigned turn=0 ; turn<Match::numberOfTurns ; ++turn)
+	{
+		leftDecisions.push_back(   leftPlayer.strategy->makeDecision(leftDecisions,  rightDecisions) );
+		rightDecisions.push_back( rightPlayer.strategy->makeDecision(rightDecisions, leftDecisions)  );
+		leftPayoff.push_back(     PayoffComputer::left (leftDecisions.back(), rightDecisions.back()) );
+		rightPayoff.push_back(    PayoffComputer::right(leftDecisions.back(), rightDecisions.back()) );
+	}
+	return;
+}
