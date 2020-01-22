d0cdc984d5ac922f62af3b29c4d09d76839be89b
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2020-01-22 10:11:32 -0300

    Test for attribute 'name' in PrisonersDilemmaFile done.

--- PrisonersDilemmaFileLibTest/source/prisonersDilemmaFileTest.cpp
+++ PrisonersDilemmaFileLibTest/source/prisonersDilemmaFileTest.cpp
@@ -26,11 +26,27 @@ TestCase("Save championship basic data", "[PrisonersDilemmaFile]")
 			&tft
 		}
 	);
+	// Create file
 	const char * filePath = ".PrisonersDilemmaFileTest_save_championship_basic_data.h5";
 	PrisonersDilemmaFile file(filePath);
-	file.save(championship);
-	file.close();
+	requireNoThrow( file.save(championship) );
+	requireNoThrow( file.close() );
 	check( std::filesystem::exists(filePath) );
+	// open file
+	H5::H5File roFile;
+	H5::Group group;
+	requireNoThrow( roFile.openFile(filePath, H5F_ACC_RDONLY)   );
+	requireNoThrow( group = roFile.openGroup(championship.name) );
+	section("name")
+	{
+		std::string attrData;
+		H5::Attribute attribute;
+		requireNoThrow( attribute = group.openAttribute("name") );
+		requireNoThrow( attribute.read(attribute.getStrType(), attrData) );
+		requireNoThrow( attribute.close() );
+		check( attrData == championship.name);
+	}
+	requireNoThrow( group.close() );
 	std::filesystem::remove(filePath);
 	checkFalse( std::filesystem::exists(filePath) );
 	return;
