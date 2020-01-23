11935c17905e46e5cac178a435d61bf339d79b95
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2020-01-23 12:27:27 -0300

    PrisonersDilemmaFileLib::save(Player) tested.
    Only one player was tested.
    It is not that bad, but it could improve
    a lot by adding more tests and refactoring
    the code.

--- PrisonersDilemmaFileLibTest/source/prisonersDilemmaFileTest.cpp
+++ PrisonersDilemmaFileLibTest/source/prisonersDilemmaFileTest.cpp
@@ -114,7 +114,7 @@ TestCase("Save championship basic data", "[PrisonersDilemmaFile]")
 	return;
 }
 
-TestCase("Save player", "[PrisonersDilemmaFile]")
+TestCase("Save player basic description", "[PrisonersDilemmaFile]")
 {
 	const unsigned numberOfTurns = 5;
 	Championship championship(
@@ -129,7 +129,7 @@ TestCase("Save player", "[PrisonersDilemmaFile]")
 	);
 	championship.compete();
 	// Create file
-	std::string filePath = ".PrisonersDilemmaFileTest_save_player.h5";
+	std::string filePath = ".PrisonersDilemmaFileTest_save_player_basic_data.h5";
 	PrisonersDilemmaFile file(filePath);
 	requireNoThrow( file.save(championship) );
 	requireNoThrow( file.close() );
@@ -175,6 +175,112 @@ TestCase("Save player", "[PrisonersDilemmaFile]")
 			check( attrData == allC.description );
 		}
 		requireNoThrow( strategyGroup.close() );
+		requireNoThrow( playerGroup.close() );
+	}
+	// Close and delete file
+	requireNoThrow( roFile.close() );
+	std::filesystem::remove(filePath);
+	checkFalse( std::filesystem::exists(filePath) );
+	return;
+}
+
+TestCase("Save player competitions", "[PrisonersDilemmaFile]")
+{
+	const unsigned numberOfTurns = 5;
+	Championship championship(
+		"Save Championship - Basic",
+		"Save Championship basic data.",
+		numberOfTurns,
+		{
+			&allC,
+			&allD,
+			&tft
+		}
+	);
+	championship.compete();
+	// Create file
+	std::string filePath = ".PrisonersDilemmaFileTest_save_player_competition_result.h5";
+	PrisonersDilemmaFile file(filePath);
+	requireNoThrow( file.save(championship) );
+	requireNoThrow( file.close() );
+	check( std::filesystem::exists(filePath) );
+	// Load file
+	PrisonersDilemmaFile roFile(filePath, H5F_ACC_RDONLY);
+	section("always cooperate player")
+	{
+		H5::Group playerGroup;
+		std::string playerGroupPath = "/" + championship.name + "/" + "Always Cooperate" + "/";
+		requireNoThrow( playerGroup = roFile.openGroup(playerGroupPath) );
+		section("competition with always defect")
+		{
+			H5::Group partnerGroup;
+			std::string partnerGroupPath = playerGroupPath + "Always Defect" + "/";
+			requireNoThrow( partnerGroup = roFile.openGroup(partnerGroupPath) );
+			section("decisions")
+			{
+				std::string datasetName = "decisions";
+				H5::DataSet dataset;
+				requireNoThrow(dataset = partnerGroup.openDataSet(datasetName) );
+				std::vector<Decision> datasetData;
+				datasetData.resize(numberOfTurns);
+				requireNoThrow( dataset.read(datasetData.data(), H5::PredType::NATIVE_UCHAR) );
+				std::vector<Decision> correctDatasetData(numberOfTurns, Decision::cooperate);
+				check( datasetData == correctDatasetData );
+				requireNoThrow( dataset.close() );
+			}
+			section("payoff")
+			{
+				std::string datasetName = "payoff";
+				H5::DataSet dataset;
+				requireNoThrow(dataset = partnerGroup.openDataSet(datasetName) );
+				std::vector<Payoff> datasetData;
+				datasetData.resize(numberOfTurns);
+				requireNoThrow( dataset.read(datasetData.data(), H5::PredType::NATIVE_UINT) );
+				std::vector<Payoff> correctDatasetData(numberOfTurns, Payoff::suckers);
+				check( datasetData == correctDatasetData );
+				requireNoThrow( dataset.close() );
+			}
+			section("partner player link and its strategy")
+			{
+				check( partnerGroup.exists("Always Defect") );
+				check( partnerGroup.exists("Always Defect/strategy") );
+				H5::Group partnerStrategyGroup;
+				std::string partnerStrategyGroupPath = partnerGroupPath + "Always Defect" + "/" + "strategy" + "/";
+				requireNoThrow( partnerStrategyGroup = roFile.openGroup(partnerStrategyGroupPath) );
+				section("name")
+				{
+					std::string attrName = "name";
+					std::string attrData;
+					H5::Attribute attribute;
+					requireNoThrow( attribute = partnerStrategyGroup.openAttribute(attrName) );
+					requireNoThrow( attribute.read(attribute.getStrType(), attrData) );
+					requireNoThrow( attribute.close() );
+					check( attrData == allD.name );
+				}
+				section("short name")
+				{
+					std::string attrName = "shortName";
+					std::string attrData;
+					H5::Attribute attribute;
+					requireNoThrow( attribute = partnerStrategyGroup.openAttribute(attrName) );
+					requireNoThrow( attribute.read(attribute.getStrType(), attrData) );
+					requireNoThrow( attribute.close() );
+					check( attrData == allD.shortName );
+				}
+				section("description")
+				{
+					std::string attrName = "description";
+					std::string attrData;
+					H5::Attribute attribute;
+					requireNoThrow( attribute = partnerStrategyGroup.openAttribute(attrName) );
+					requireNoThrow( attribute.read(attribute.getStrType(), attrData) );
+					requireNoThrow( attribute.close() );
+					check( attrData == allD.description );
+				}
+				requireNoThrow( partnerStrategyGroup.close() );
+			}
+			requireNoThrow( partnerGroup.close() );
+		}
 		requireNoThrow( playerGroup.close() );
 	}
 	// Close and delete file
