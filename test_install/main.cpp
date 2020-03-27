4c4b18681431f3c01663a6a48919314cdef674ac
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2020-03-27 12:36:59 -0300

    Adding code for testing the project.
    The installation configuration was not
    yet done so the 'test_install' won't
    work.
    
    It was added so when the installation
    is done, it will be possible to test it.

---
+++ test_install/main.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <iostream>
+#include <Championship.hpp>
+#include <PrisonersDilemmaFile.hpp>
+
+int main()
+{
+	// Championship
+	const unsigned numberOfTurns = 50;
+	PrisonersDilemma::Championship championship(
+		{
+			"All strategies",
+			"Competition among all strategies.",
+			numberOfTurns
+		},
+		{
+			&PrisonersDilemma::allC   ,
+			&PrisonersDilemma::allD   ,
+			&PrisonersDilemma::moon   ,
+			&PrisonersDilemma::grim   ,
+			&PrisonersDilemma::pvl    ,
+			&PrisonersDilemma::gradual,
+			&PrisonersDilemma::sm     ,
+			&PrisonersDilemma::hm     ,
+			&PrisonersDilemma::np     ,
+			&PrisonersDilemma::rp     ,
+			&PrisonersDilemma::sg     ,
+			&PrisonersDilemma::pb     ,
+			&PrisonersDilemma::fbf    ,
+			&PrisonersDilemma::tft    ,
+			&PrisonersDilemma::tftt   ,
+			&PrisonersDilemma::ttft   ,
+			&PrisonersDilemma::rtft   ,
+			&PrisonersDilemma::gtft   ,
+			&PrisonersDilemma::stft   ,
+			&PrisonersDilemma::htft   ,
+			&PrisonersDilemma::atft   ,
+			&PrisonersDilemma::mratft ,
+		}
+	);
+	championship.compete();
+	championship.rank();
+	std::cout << championship;
+
+	//Save result in file
+	PrisonersDilemma::File file("all_strategies.prisonersdilemma", championship);
+	
+	return 0;
+}
